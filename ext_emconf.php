adc6827e55392a0e4c4b1cad387b822bf92bfad8
Rune Piper <kontakt@runepiper.de>
2018-11-28 15:30:15 +0100

    [TASK] Raise contraints, refs #3

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,12 +7,12 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'visuellverstehen',
     'author_email' => 'kontakt@visuellverstehen.de',
     'author_company' => 'visuellverstehen',
-    'state' => 'stable',
+    'state' => 'beta',
     'clearCacheOnLoad' => false,
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
         ]
     ]
 ];
