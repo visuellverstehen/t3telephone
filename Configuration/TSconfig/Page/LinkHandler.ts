322670201a2fcded51db211e8c167e311a1e4d6f
Rune Piper <kontakt@runepiper.de>
2018-11-30 14:43:22 +0100

    [CLEANUP] Move TS to ext_localconf.php

--- Configuration/TSconfig/Page/LinkHandler.ts
+++
@@ -1,8 +0,0 @@
-TCEMAIN.linkHandler {
-    tel {
-        handler = VV\T3telephone\Recordlist\LinkHandler\TelLinkHandler
-        label = LLL:EXT:t3telephone/Resources/Private/Language/locallang_browse_links.xlf:tel
-        scanAfter = page
-        addParams = onclick="jumpToUrl('?act=tel&linkAttributes[title]=Call number&linkAttributes[class]=phone&linkAttributes[params]=');return false;"
-    }
-}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,18 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['linkHandler']['tel'] = \VV\T3telephone\LinkHandling\TelLinkHandler::class;
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['linkHandler']['tel'] =
+    \VV\T3telephone\LinkHandling\TelLinkHandler::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typolinkLinkHandler']['tel'] =
+    \VV\T3telephone\Hooks\TypoLinkHandler::class;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3telephone/Configuration/TSconfig/Page/LinkHandler.ts">'
-);
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typolinkLinkHandler']['tel'] = \VV\T3telephone\Hooks\TypoLinkHandler::class;
+// Register new link handler
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    TCEMAIN.linkHandler {
+        tel {
+            handler = VV\\T3telephone\\Recordlist\\LinkHandler\\TelLinkHandler
+            label = LLL:EXT:t3telephone/Resources/Private/Language/locallang_browse_links.xlf:tel
+            scanAfter = page
+        }
+    }
+');
